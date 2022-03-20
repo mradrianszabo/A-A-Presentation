cc14558732baf7b216bb46bed4278e50a7500b32
mradrianszabo <mradrianszabo@gmail.com>
2022-03-20 08:23:58 +0100

    basic structure introduced

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrameComponent } from './frame/frame.component';
+import { ExampleComponent } from './example/example.component';
+import { ModuleComponent } from './module/module.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrameComponent,
+    ExampleComponent,
+    ModuleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
