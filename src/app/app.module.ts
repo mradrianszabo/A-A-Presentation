9de231fd98ee9cebaa58341439b8434aa1cce802
mradrianszabo <mradrianszabo@gmail.com>
2022-03-20 12:05:22 +0100

    basics set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrameComponent } from './frame/frame.component';
 import { ExampleComponent } from './example/example.component';
 import { ModuleComponent } from './module/module.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { Module1Component } from './module1/module1.component';
+import { Module2Component } from './module2/module2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrameComponent,
     ExampleComponent,
-    ModuleComponent
+    ModuleComponent,
+    Module1Component,
+    Module2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
