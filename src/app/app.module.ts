f47a37556924b455d275e09bf966f3295ee5079f
mradrianszabo <mradrianszabo@gmail.com>
2022-04-08 10:04:49 +0200

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrameComponent } from './frame/frame.component';
 import { ExampleComponent } from './example/example.component';
-import { ModuleComponent } from './module/module.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { Module1Component } from './module1/module1.component';
 import { Module2Component } from './module2/module2.component';
-import { Object1Component } from './module2/object1/object1.component';
 import { RouterModule, Routes } from '@angular/router';
 import { Module3Component } from './module3/module3.component';
 
@@ -27,10 +25,8 @@ const routes: Routes = [
     AppComponent,
     FrameComponent,
     ExampleComponent,
-    ModuleComponent,
     Module1Component,
     Module2Component,
-    Object1Component,
     Module3Component
   ],
   imports: [
